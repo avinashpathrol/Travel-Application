dce62a0050ded08e856471d6eb43cf11a584fb22
Avinash <avinash.pathrol@titanlogix.com>
2024-04-02 19:56:12 -0600

    Install and import cors package

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
-// Other requires...
+const cors = require('cors');
+const cityRoutes = require('../Travel Reservation/routes/cityRoutes');
+const weatherRoutes = require('../Travel Reservation/routes/weatherRoutes');
 
 const app = express();
 
