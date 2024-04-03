9eb5b3e222960639e24d709ae4af38f28a0a702f
Avinash <avinash.pathrol@titanlogix.com>
2024-04-02 20:03:09 -0600

    Fix path issues

--- server.js
+++ server.js
@@ -1,27 +1,19 @@
 const express = require('express');
-const path = require('path');
+const cityRoutes = require('./routes/cityRoutes');
+const weatherRoutes = require('./routes/weatherRoutes');
+const geocodeRoutes = require('./routes/geocodeRoutes');
 const cors = require('cors');
-const cityRoutes = require('../Travel Reservation/routes/cityRoutes');
-const weatherRoutes = require('../Travel Reservation/routes/weatherRoutes');
-
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'build')));
 
 // API routes
 app.use('/api/cities', cityRoutes);
 app.use('/weather', weatherRoutes);
 //app.use('/geo', geocodeRoutes);
 
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/build/index.html'));
-});
 
 const PORT = process.env.PORT || 3001;
 
